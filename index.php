9f2c5bcc659126139245694bce669b9e0c9a1250
Sam Khoo <samkhoo77@gmail.com>
2017-09-08 00:03:36 +0800

    removed unecessary stuffs

--- index.php
+++ index.php
@@ -3,17 +3,7 @@
 $page_header = 'Home';
 include_once('header.php');
 
-include_once('classes/database.class.php');
-$thelink = DatabaseConnection::create_connection("localhost", "root", "", "hackathon");
 
-$name = 21;
-$query = $thelink->query(
-        "SELECT * FROM test WHERE name = ?;",
-          array('s', $name)
-        );
-
-
-var_dump($query)
 
 
 ?>
