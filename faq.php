b2a182966d4144cf8b7331d3270870068d41bad3
meaniechee <jasminechee.mc@gmail.com>
2017-09-08 23:33:33 +0800

    faq icon update

---
+++ faq.php
@@ -0,0 +1,108 @@
+<?php 
+	$page_header = 'How It Works';
+	include_once('header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- PAGE TITLE SECTION -->
+<section class="clearfix pageTitleSection" style="background-image: url(how-it-works.html);">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="pageTitle">
+					<h2>how it works</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- HOW WORKS SECTION -->
+<section class="clearfix howWorksSection">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="howWorksInner">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="howWorksImage text-center"><img src="img/works/works-big-1.png" alt="Image Works"></div>
+						</div>
+						<div class="col-xs-12">
+							<div class="howWorksInfo text-center">
+								<p>Step 1</p>
+								<h3>Find A Parking Spot</h3>
+								<p>Use our ‘Quick Find’ feature or search manually</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="howWorksInner">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="howWorksImage text-center"><img src="img/works/works-big-2.png" alt="Image Works"></div>
+						</div>
+						<div class="col-xs-12">
+							<div class="howWorksInfo text-center">
+								<p>Step 2</p>
+								<h3>Pick Your Sweet Spot</h3>
+								<p>Look around on our handy map and check out the available listings</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="howWorksInner">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="howWorksImage text-center"><img src="img/works/works-big-3.png" alt="Image Works"></div>
+						</div>
+						<div class="col-xs-12">
+							<div class="howWorksInfo text-center">
+								<p>Step 3</p>
+								<h3>‘Chop’ the Spot!</h3>
+								<p>Proceed to ‘Book Now’ and pay online. It’s as easy as 123!</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+	include_once('footer.php');
+?>
